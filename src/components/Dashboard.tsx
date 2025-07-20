47da992985edb59563e1a1752742710e90231938
Luis-EncoraDev <luis.arguelles@encora.com>
2025-07-19 22:38:31 -0500

    Created login component. Tested requests to Spring Boot app

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,31 @@
-
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
+    const [topArtists, setTopArtists] = useState();
+    const token = localStorage.getItem("jwt");
+
+    const getTopArtists = async () => {
+        const response = await axios.get("http://localhost:9090/api/me/top/artists", {
+            withCredentials: true,
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+        }
+        })
+        setTopArtists(response.data.items);
+    }
+
+    useEffect(() => {
+        getTopArtists();
+    }, []);
+
+    useEffect(() => {
+    console.log("Response:", topArtists);
+    }, [topArtists]);
+
     return(
-        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+        <div className="h-screen flex items-center justify-center bg-gradient-to-r from-[#32a88d] to-black">
             <h1 className="text-4xl font-bold text-white">Hello Tailwind + Vite + TS!</h1>
         </div>
     )
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,23 @@
+import { Button } from "@mui/material";
 
 const Login = () => {
-
     const loginWithSpotify = async () => {
         window.location.href = "http://127.0.0.1:9090/oauth2/authorization/spotify";
     }
 
     return(
-        <div>
-            <p>
+        <div className="flex flex-col w-[25%] py-[10%] justify-self-center items-center rounded mt-[8%] border-2 border-[#32a88d] gap-18">
+            <img src="/spotify.png" width="80px" height="80px" />
+            <p className="font-mono text-[30px] font-bold">Login with Spotify</p>
+            <Button variant="outlined" sx={{ 
+                borderRadius: "8px",
+                width: "50%",
+                padding: "20px"
+            }}
+            onClick={loginWithSpotify}
+            >
                 Login
-            </p>
-            <button onClick={loginWithSpotify}>
-                Login with Spotify
-            </button>
+            </Button>
         </div>
     )
 }
